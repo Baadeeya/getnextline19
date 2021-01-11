4fd0cd23316655388ec21d3963d5b855786108c4
David Gutin <david14av@gmail.com>
2021-01-11 19:52:01 +0100

    getting somewhere

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 17:15:51 by dgutin            #+#    #+#             */
-/*   Updated: 2021/01/11 18:16:23 by dgutin           ###   ########.fr       */
+/*   Updated: 2021/01/11 19:51:41 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
-int	ft_strlen(char *str)
+int		ft_strlen(char *str)
 {
 	int i;
 
@@ -24,6 +24,51 @@ int	ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+int		ft_linebool(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		if (str[i] == '\n')
+			return (i);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (!(sub = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len)
+		sub[i++] = s[start++];
+	sub[i] = '\0';
+	return (sub);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t		i;
+	const char	*psrc;
+	char		*pdst;
+
+	i = -1;
+	psrc = src;
+	pdst = dst;
+	if (dst == 0 && src == dst)
+		return (0);
+	while (++i < n)
+		*(pdst + i) = *(psrc + i);
+	return (dst);
 }
 
 char	*ft_strjoined(char const *s1, char const *s2)
@@ -44,19 +89,27 @@ char	*ft_strjoined(char const *s1, char const *s2)
 	return (cat);
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	int				i;
-	int				x;
-	char			*buf;
-	static char		*cat;
+	int			i;
+	int			o;
+	char		*buf;
+	char		*tmp;
+	static char	*cat;
 
-	i = -1;
-	x = -1;
+	i = 0;
+	x = 0;
 	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	while (cat)
+	while (!ft_linebool(cat))
 	{
-		read(fd, buf, BUFFER_SIZE);
+		o = (int)read(fd, buf, BUFFER_SIZE);
+		buf[o + 1] = '\0';
 		cat = ft_strjoined(cat, buf);
 	}
+	i = ft_linebool(&line);
+	*line = (char *)malloc(sizeof(char) * i + 1);
+	ft_memcpy(*line, cat, i);
+	tmp = ft_substr(cat, i, o - i);
+	free(cat);
+	cat = ft_substr(tmp, 0, o - i);
 }
